34726d388229410105b5833804ab3020370db371
Stefina <stefina1109@gmail.com>
2025-02-08 19:24:44 +0530

    Time: 4 ms (32.87%) | Memory: 12.4 MB (47.16%) - LeetSync

---
+++ 65-valid-number/valid-number.py
@@ -0,0 +1,7 @@
+import re
+
+class Solution:
+    def isNumber(self, s):
+        # Regular expression to match a valid number
+        pattern = r'^[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?$'
+        return bool(re.match(pattern, s.strip()))
